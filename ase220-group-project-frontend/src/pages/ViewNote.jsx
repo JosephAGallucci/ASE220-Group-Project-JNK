84df3c34302b9bd09d68b16b1f57c8425eb8baec
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2026-05-04 16:37:28 -0400

    Fix viewnote setNote function

--- ase220-group-project-frontend/src/pages/ViewNote.jsx
+++ ase220-group-project-frontend/src/pages/ViewNote.jsx
@@ -3,25 +3,26 @@ import Header from "../components/Header.jsx";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-export default function ViewNote()
-{
-    const {id} = useParams();
-    const {note, setNote} = useState(null);
+export default function ViewNote() {
+    const { id } = useParams();
+    const [note, setNote] = useState(null);
 
     useEffect(() => {
-        fetch (`/API/notes/${id}`)
-        .then ((response) => response.json())
-        .then ((response) => {
-            setNote(response);
-        })
-        .catch((error) => console.error(error));
-    }
-    );
+        fetch(`/API/notes/${id}`)
+            .then((response) => response.json())
+            .then((response) => {
+                if (response.id) setNote(response);
+            })
+            .catch((error) => console.error(error));
+    }, []);
 
     return (
         <>
-        <Header />
-        <LargeNote key={id} note={note}/> 
+            <Header />
+            {note ?
+                <LargeNote key={id} note={note} /> :
+                <p style={{ margin: "auto" }}>There is no note for this id</p>
+            }
         </>
     );
 }
