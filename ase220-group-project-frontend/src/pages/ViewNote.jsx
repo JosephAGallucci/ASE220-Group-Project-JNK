9e512dce0545a729656d0a0abe34f815deeab5ae
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2026-05-04 18:33:04 -0400

    Make delete button work

--- ase220-group-project-frontend/src/pages/ViewNote.jsx
+++ ase220-group-project-frontend/src/pages/ViewNote.jsx
@@ -1,11 +1,29 @@
 import LargeNote from "../components/LargeNote.jsx";
 import Header from "../components/Header.jsx";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useToken } from "../context/AuthContext.jsx";
 
 export default function ViewNote() {
     const { id } = useParams();
     const [note, setNote] = useState(null);
+    const { token } = useToken();
+    const navigate = useNavigate();
+
+    const tokenData = JSON.parse(atob(token.split('.')[1]));
+
+    const deleteNote = () => {
+        fetch(`/API/notes/${id}`, {
+            method: 'DELETE',
+            headers: { authorization: `Bearer ${token}` }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    navigate('/dashboard');
+                }
+            })
+            .catch((error) => console.error(error));
+    }
 
     useEffect(() => {
         fetch(`/API/notes/${id}`)
@@ -19,10 +37,16 @@ export default function ViewNote() {
     return (
         <>
             <Header />
-            {note ?
-                <LargeNote key={id} note={note} /> :
-                <p style={{ margin: "auto" }}>There is no note for this id</p>
-            }
+            <div className="create-form">
+                {note ?
+                    <>
+                        <LargeNote key={id} note={note} />
+                        {tokenData.sub === note.owner && <button onClick={deleteNote} className="deletebutton">Delete</button>}
+                    </>
+                    :
+                    <p style={{ margin: "auto" }}>There is no note for this id</p>
+                }
+            </div>
         </>
     );
 }
