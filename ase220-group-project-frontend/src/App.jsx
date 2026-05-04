25a8e4cb55f7918688b056b202c7e37b3332ab99
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2026-05-04 11:28:31 -0400

    Add Register page

--- ase220-group-project-frontend/src/App.jsx
+++ ase220-group-project-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx';
 import Home from './pages/Home.jsx';
 
 
 const router = createBrowserRouter([
   { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> },
   { path: '/', element: <Home /> }
 ]);
 
