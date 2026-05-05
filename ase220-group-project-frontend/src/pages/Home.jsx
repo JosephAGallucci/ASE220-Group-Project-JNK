37b38478963d7187830f0e3702a723c66fc1f278
JosephAGallucci <josephagallucci38@gmail.com>
2026-05-05 08:47:52 -0400

    Searchbar placeholder

--- ase220-group-project-frontend/src/pages/Home.jsx
+++ ase220-group-project-frontend/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import Dashboard from "../components/Dashboard.jsx";
+import SearchBar from "../components/SearchBar.jsx";
 
 export default function Home() {
   const [notes, setNotes] = useState(null);
+  const [filterText, setFilterText] = useState("");
 
   useEffect(() => {
     fetch('/API/notes').then(res => res.json()).then((setNotes));
@@ -10,6 +12,9 @@ export default function Home() {
 
 
   return (
+    <>
+    <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
     <Dashboard notes={notes} />
+    </>
   )
 }
