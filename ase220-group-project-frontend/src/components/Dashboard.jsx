332e257db389e71264447c8d9b46d8cb1b63ee45
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2026-05-05 09:38:35 -0400

    Add SeachBar component and put it in the Dashboard
    Co-authored-by: JosephAGallucci <josephagallucci38@gmail.com>

--- ase220-group-project-frontend/src/components/Dashboard.jsx
+++ ase220-group-project-frontend/src/components/Dashboard.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
+import SearchBar from "../components/SearchBar.jsx";
 import NoteGrid from "../components/NoteGrid.jsx";
 import TagFilter from "../components/TagFilter.jsx";
 import Header from "../components/Header.jsx";
 
+/**
+ * Dashboard component that displays a collection of notes.
+ *
+ * @component
+ * @param {Object} props
+ * @param {Array<{id: string, title: string, content: string, owner: string, tag: string}>} props.notes - List of notes to display.
+ * @returns {JSX.Element}
+ */
 export default function Dashboard({ notes }) {
     const [filter, setFilter] = useState({ science: true, math: true, history: true, music: true, art: true, english: true, other: true });
+    const [search, setSearch] = useState("");
 
-    const renderedNotes = notes?.filter((note) => filter[note.tag]);
+    const renderedNotes = notes?.filter((note) => filter[note.tag] && note.title.toLowerCase().includes(search.toLowerCase()));
 
     return (
         <>
             <Header />
+            <SearchBar filterText={search} onFilterTextChange={setSearch} />
             <div style={{ display: 'flex' }}>
                 <NoteGrid notes={renderedNotes} />
                 <TagFilter filter={filter} setFilter={setFilter} />
